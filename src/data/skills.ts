708d933fa537849861360aa384faf209289fa439
WanAdamm <wanadamworkmail@gmail.com>
2026-05-28 17:56:44 +0800

    update portfolio

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -5,8 +5,8 @@ export const languageSkills = [
   { name: "SQL", level: 80 },
 ];
 
-export const aiDataSkills = ["FastAPI", "PyTorch", "TensorFlow", "Scikit-Learn", "Pandas"];
+export const aiDataSkills = ["FastAPI", "PyTorch", "TensorFlow", "Scikit-Learn", "Pandas", "Matplotlib", "Seaborn"];
 
-export const frontendSkills = ["React.js", "Astro", "Flutter", "Tailwind CSS"];
+export const frontendSkills = ["React.js","Vite", "Astro", "Flutter", "Tailwind CSS"];
 
-export const devopsSkills = ["Docker", "Git", "GCP", "CI/CD"];
+export const devopsSkills = ["Docker", "Git", "Firebase", "Azure", "CI/CD"];
